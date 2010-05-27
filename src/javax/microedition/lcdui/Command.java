a80d4072951d803fc1b9f9985df8680b2ad017fe
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-05-27 15:17:03 +0200

    Implemented Command#toString to make Command objects easily usable in the Alert implementation.

--- src/javax/microedition/lcdui/Command.java
+++ src/javax/microedition/lcdui/Command.java
@@ -4,5 +4,19 @@ public class Command
     {
     public Command( final String aLabel, final int aType, final int aPriority )
         {
+        myLabel = aLabel;
+        myType = aType;
+        myPriority = aPriority;
         }
+
+    public String toString()
+        {
+        return myLabel;
+        }
+
+    private final int myType;
+
+    private final int myPriority;
+
+    private final String myLabel;
     }
